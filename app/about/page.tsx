8e42b76aabf50ca4707997b6bafe363475e57f03
akigler <53454160+akigler@users.noreply.github.com>
2025-10-06 14:37:54 -0400

    fixed about footer

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,5 @@
 import { Navigation } from "@/components/navigation"
+import { Footer } from "@/components/Footer"
 import { FAQItem } from "@/components/faq-item"
 import { BuyCoinsButton } from "@/components/buy-coins-button"
 import { Coins } from "lucide-react"
